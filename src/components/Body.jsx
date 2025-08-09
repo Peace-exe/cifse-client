6757aeea5aec6a64259a22ebb8efb751a0e52934
Aman Yadav <amanyadav2303@gmail.com>
2025-08-10 01:01:04 +0530

    footer

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,6 +4,7 @@ import AboutSection from "./AboutUs";
 import Courses from "./Courses";
 import WhyChooseUs from "./WhyChooseUs";
 import Contact from "./Contact";
+import Footer from "./Footer";
 
 const Body = ()=>{
     return <div className="flex flex-col min-h-screen">
@@ -16,7 +17,7 @@ const Body = ()=>{
         <Contact/>
         
     </div>
-    
+    <Footer/>
     </div>
 }
 
